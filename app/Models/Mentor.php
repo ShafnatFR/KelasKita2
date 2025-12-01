c181469f5ffb49b6af4785751601efb1ade51326
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-11-30 18:22:07 -0800

    fIX

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -7,13 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Mentor extends Model
 {
     protected $table = 'tb_mentor';
-<<<<<<< HEAD
-    protected $fillable = ['user_id', 'status'];
-=======
 
     protected $fillable = [
         'user_id',
         'status',
     ];
->>>>>>> 7963d1fc0c914805c72044fdcdd1c1aaac1cc9af
 }
